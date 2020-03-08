a66e971825784faa51ed7752cee3d36bcdf1ec57
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2020-03-08 12:27:17 +0700

    Scraper tested; works

--- scrape_song.py
+++ scrape_song.py
@@ -1,2 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
 def scrape_song(search_input):
-    return '...'
+    """ Input:  String 
+                Search Term for artist or song
+        Output: Dictionary
+                Information for the song. If it's an artist, for the first song found. 
+    """
+    
+    # BASE_URL = 'https://www.azlyrics.com/'
+    SEARCH_URL = 'https://search.azlyrics.com/search.php?q='
+    req = requests.get(SEARCH_URL)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    print(soup.prettify())
+    
