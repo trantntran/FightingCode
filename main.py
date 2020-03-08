b29f951ff9ae1198a51eb93263c8a06bf5d6bfea
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2020-03-08 12:14:17 +0700

    Finished form and added scraping file

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+from scrape_song import scrape_song
+
+
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home.html')
+    search_input = None
+    if request.method == 'POST':
+      search_input = request.form.get('search_input')
 
+    data = scrape_song(search_input) if search_input != None else None
+    return render_template('home.html', data=data)
+    
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
  
---
+++ scrape_song.py
@@ -0,0 +1,2 @@
+def scrape_song(search_input):
+    return '...'
