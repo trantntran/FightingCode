38e54f708765b188664db36b85937643a1407be6
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2020-03-09 10:58:04 +0700

    Changed host for docker container

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def index():
     return render_template('home.html', data=data)
     
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
  
